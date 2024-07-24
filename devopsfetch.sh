35d5097f4bd4306053460c55af1d21845164aef6
zenitugo <ugochiukaegbu19@gmail.com>
2024-07-24 16:14:47 +0100

    command line tool

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -2,8 +2,6 @@
 
 # Create the DevOpsFetch script
 
-
-
 # Function to display help
 devopsfetch_help() {
     echo "Usage: devopsfetch [OPTION]..."
@@ -24,6 +22,9 @@ devopsfetch_help() {
     echo "         devopsfetch -u john"
     echo "         devopsfetch -t '2024-07-22'"
 }
+
+
+
 
 # Function to display active ports
 ports_available() {
@@ -63,8 +64,12 @@ ports_available() {
     fi
 }
 
+
+
+
 # Function to display Docker information
 docker_info() {
+    echo "##################################### List of Docker Images and Containers ######################################"
     if [ -z "$1" ]; then
         echo "Docker images:"
         docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}"
@@ -79,50 +84,65 @@ docker_info() {
 
 
 
+
 # Function to display Nginx information
 nginx_server_info() {
+    echo "##################################### Nginx Domains and Config Files ######################################"
     if [ -z "$1" ]; then
-        printf "%-30s %-30s %-30s %-50s\n" "DOMAIN" "PROXY" "CONFIGURATION" "CONFIG FILE"
-        printf "%-30s %-30s %-30s %-50s\n" "------" "-----" "-------------" "-----------"
+        # Print the table header
+        printf "+-------------------------------+-------------------------------+-------------------------------+----------------------------------------------------+\n"
+        printf "| %-30s | %-29s | %-29s | %-49s |\n" "DOMAIN" "PROXY" "CONFIGURATION" "CONFIG FILE"
+        printf "+-------------------------------+-------------------------------+-------------------------------+----------------------------------------------------+\n"
         
+        # Loop through the Nginx configuration files
         for config in /etc/nginx/sites-enabled/*; do
             if [ -f "$config" ]; then
+                # Extract domain, proxy, and listen information
                 domain=$(grep -m1 "server_name" "$config" | awk '{print $2}' | sed 's/;$//')
                 proxy=$(grep -m1 "proxy_pass" "$config" | awk '{print $2}' | sed 's/;$//')
                 listen=$(grep -m1 "listen" "$config" | awk '{print $2}' | sed 's/;$//')
-                root=$(grep -m1 "root" "$config" | awk '{print $2}' | sed 's/;$//')
                 
                 config_info="listen: $listen"
-                [ ! -z "$root" ] && config_info="$config_info, root: $root"
                 
-                printf "%-30s %-30s %-30s %-50s\n" "$domain" "${proxy:-N/A}" "$config_info" "$config"
+                # Print the row for each configuration file
+                printf "| %-30s | %-29s | %-29s | %-49s |\n" "${domain:-N/A}" "${proxy:-<No Proxy>}" "$config_info" "$config"
             fi
         done
+
+        # Print the table footer
+        printf "+-------------------------------+-------------------------------+-------------------------------+----------------------------------------------------+\n"
     else
         echo "Nginx configuration for domain $1:"
         grep -A 20 -R "server_name $1" /etc/nginx/sites-enabled/* | sed -n '/server_name/,$p'
     fi
 }
-  
+
+
+
+
+
+
 
 
 # Function to display user information
 get_users() {
+     echo "##################################### List of Regular Users ######################################"
     if [ -z "$1" ]; then
-        # Display all regular users
-        printf "%-15s %-10s %s\n" "Users" "Status" "Last-Login"
-        echo "-----------------------------------------------"
+        printf "+-----------------+------------+-------------------------------+\n"
+        printf "| %-15s | %-10s | %-30s |\n" "Users" "Status" "Last-Login"
+        printf "+-----------------+------------+-------------------------------+\n"
     
         awk -F: '$3 >= 1000 { print $1 }' /etc/passwd | while read user; do
             status="Regular"
-        
             last_login=$(last -n 1 "$user" | head -n 1 | awk '{print $4, $5, $6, $7, $8, $9}')
             if [[ -n "$last_login" ]]; then
-                printf "%-15s %-10s %s\n" "$user" "$status" "$last_login"
+                printf "| %-15s | %-10s | %-30s |\n" "$user" "$status" "$last_login"
             else
-                printf "%-15s %-10s %s\n" "$user" "$status" "Never logged in"
+                printf "| %-15s | %-10s | %-30s |\n" "$user" "$status" "Never logged in"
             fi
         done
+        
+        printf "+-----------------+------------+-------------------------------+\n"
     else
         # Display details for a specific user
         if id "$1" &>/dev/null; then
@@ -144,6 +164,7 @@ get_users() {
         fi
     fi
 }
+
 
 
 # Function to filter system logs based on time range
